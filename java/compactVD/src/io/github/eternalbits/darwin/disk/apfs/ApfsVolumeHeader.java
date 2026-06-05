54564d5c527228e6c261878548c0e59361a3578a
eternalbits <eternalbits@users.noreply.github.com>
2026-06-05 12:07:34 +0100

    Lower nx_xp_desc_blocks minimum from 32 to 8
    Relax the validation in ApfsVolumeHeader.java by changing the lower bound for nx_xp_desc_blocks from >32 to >8. This allows APFS volumes with fewer xp descriptor blocks to be recognized while keeping the existing upper bound (<512) and other sanity checks unchanged.

--- java/compactVD/src/io/github/eternalbits/darwin/disk/apfs/ApfsVolumeHeader.java
+++ java/compactVD/src/io/github/eternalbits/darwin/disk/apfs/ApfsVolumeHeader.java
@@ -101,7 +101,7 @@ public class ApfsVolumeHeader {
 				if (nx_type == CONTAINER && nx_subtype == 0 
 						&& nx_magic == SUPERBLOCK_MAGIC && Static.isPower2(nx_block_size) 
 						&& nx_block_count * nx_block_size == fileSystem.getLength()
-						&& nx_xp_desc_blocks > 32 && nx_xp_desc_blocks < 512) {
+						&& nx_xp_desc_blocks > 8 && nx_xp_desc_blocks < 512) {
 					
 					HEADER_XID = nx_xid;
 					return;
