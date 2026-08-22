2cf79bb80f9dfc04026f0d604c6be409be69e3ec
eternalbits <eternalbits@users.noreply.github.com>
2026-08-22 14:29:35 +0100

    Handle file paths with spaces in CLI args

--- java/compactVD/src/io/github/eternalbits/compactvd/CompactVD.java
+++ java/compactVD/src/io/github/eternalbits/compactvd/CompactVD.java
@@ -246,11 +246,12 @@ public class CompactVD implements DiskImageObserver {
 			if (new File(parm[j]).exists()) {
 				done = true;
 				j = i + 1;
-			} else
-			if (i + 1 < parm.length) {
-				parm[j] += " " + parm[i + 1];
-				parm[i + 1] = null;
+			} else {
 				done = false;
+				if (i + 1 < parm.length) {
+					parm[j] += " " + parm[i + 1];
+					parm[i + 1] = null;
+				}
 			}
 		}
 		return done;
